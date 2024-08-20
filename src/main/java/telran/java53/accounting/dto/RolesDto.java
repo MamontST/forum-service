182fe5e4edc9d7cf4dd10f12605758defb3e691a
mamontair <mamontair@localhost>
2024-08-20 21:00:05 +0300

    accounting dto

---
+++ src/main/java/telran/java53/accounting/dto/RolesDto.java
@@ -0,0 +1,18 @@
+package telran.java53.accounting.dto;
+
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RolesDto {
+	String login;
+	Set<String> roles;
+	
+}
---
+++ src/main/java/telran/java53/accounting/dto/UserEditDto.java
@@ -0,0 +1,14 @@
+package telran.java53.accounting.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEditDto {
+	
+	String firstName;
+	String lastName;
+}
