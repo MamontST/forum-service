159170d50aaeb38238451db8c4e41e6c29b9690a
mamontair <mamontair@air-mamont>
2024-08-27 20:39:11 +0300

    fix

--- src/main/java/telran/java53/post/service/ForumServiceImpl.java
+++ src/main/java/telran/java53/post/service/ForumServiceImpl.java
@@ -1,7 +1,6 @@
 package telran.java53.post.service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
